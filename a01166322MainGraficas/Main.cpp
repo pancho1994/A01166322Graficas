b00c891072974f77761f99924ab40ed3dcf2c8ac
Francisco <pacotas64@gmail.com>
2017-08-14 17:05:54 -0500

    cambios de inicio

---
+++ a01166322MainGraficas/Main.cpp
@@ -0,0 +1,18 @@
+/************************************************
+
+Materia: Graficas Computacionales
+Fecha: 14 de Agosto del 2017
+Autor: A01166322 Francisco Vil lafranca
+
+************************************************/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, world" << std::endl;
+	std::cin.get();
+
+	return 0;
+
+}
