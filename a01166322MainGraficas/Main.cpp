45f8ce2518f32e7f335f5dffcb5fd7c00fba05f6
Francisco <pacotas64@gmail.com>
2017-08-23 17:04:17 -0500

    Librerias Configuradas

--- a01166322MainGraficas/Main.cpp
+++ a01166322MainGraficas/Main.cpp
@@ -7,9 +7,7 @@ Autor: A01166322 Francisco Vil lafranca
 ************************************************/
 
 #include <iostream>
-#include "Circle.h"
-#include "Rectangle.h"
-#include "Employee.h"
+
 
 using namespace std;
 
@@ -19,14 +17,5 @@ using namespace std;
 int main() 
 {
 
-	std::cout << "Fuck tha police" << std::endl;
 	
-	Circle killme(5);
-	Circle killmeAdvanced(1, "red");
-	std::cout << killme.GetRadius() << std::endl;
-	std::cout << killmeAdvanced.GetColor() << std::endl;
-	std::cin.get();
-
-	
-	return 0;
 }
