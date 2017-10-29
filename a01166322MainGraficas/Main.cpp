1bd44857e86be8d57c7d887ac299f93af278c24a
Francisco <pacotas64@gmail.com>
2017-10-29 14:32:55 -0600

    Examen v2: Francisco Villafranca A01166322 Edgar Saul Ortiz Borja A01374365

--- a01166322MainGraficas/Main.cpp
+++ a01166322MainGraficas/Main.cpp
@@ -5,102 +5,62 @@ Autor: A01166322 Francisco Villafranca
 ************************************************/
 
 #include <iostream>
+#include <memory>
 #include <GL/glew.h>
+#include <iomanip>
 #include <GL/freeglut.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 #include "InputFile.h"
 #include <vector>
 #include "Mesh.h"
 #include "ShaderProgram.h"
 #include "Shader.h"
+#include "Transform.h"
+
+
+Transform rotation;
+Transform translation;
+Transform scale;
+Transform combined;
 
 
 
-Mesh _mesh;
-ShaderProgram _shaderProgram;
-
-void Initialize()
+void PrintTransform(Transform& t) 
 {
-	std::vector<glm::vec2> positions;
-	positions.push_back(glm::vec2(-1.0f, -1.0f));
-	positions.push_back(glm::vec2(1.0f, -1.0f));
-	positions.push_back(glm::vec2(1.0f, 1.0f));
-	positions.push_back(glm::vec2(-1.0f, 1.0f));
+	glm::mat4 _modelMatrix = t.GetModelMatrix();
+	for (size_t i = 0; i < _modelMatrix.length(); i++) 
+	{
+		for (size_t j = 0; j < _modelMatrix.length(); j++) 
+		{
+			std::cout << std::setw(10) << std::left << _modelMatrix[j][i] << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
 
-	std::vector<glm::vec3> colors;
-	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
-	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
-	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
-	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
 
-	_mesh.CreateMesh(4);
-	_mesh.SetPositionAttribute(positions, GL_STATIC_DRAW, 0);
-	_mesh.SetColorAttribute(colors, GL_STATIC_DRAW, 1);
+void initialize() 
+{
+	rotation.SetRotation(45, 90, 30);
+	PrintTransform(rotation);
 
-	_shaderProgram.CreateProgram();
-	_shaderProgram.Activate();
-	_shaderProgram.AttachShader("Default.vert", GL_VERTEX_SHADER);
-	_shaderProgram.AttachShader("Default.frag", GL_FRAGMENT_SHADER);
-	_shaderProgram.SetAttribute(0, "VertexPosition");
-	_shaderProgram.SetAttribute(1, "VertexColor");
-	_shaderProgram.LinkProgram();
-	_shaderProgram.Deactivate();
+	translation.SetPosition(3, 5, -7);
+	PrintTransform(translation);
+
+	scale.SetScale(2, 3, 4);
+	PrintTransform(scale);
+
+	combined.SetRotation(45, 90, 30);
+	combined.SetPosition(3, 5, -7);
+	combined.SetScale(2, 3, 4);
+
+	PrintTransform(combined);
 
 }
 
-// Esta función se mandará a llamar para dibujar un frame.
-void GameLoop()
-{
-	// Siempre es recomendable borrar la información anterior del framebuffer.
-	// En este caso estamos borrando la información de color,
-	// y la información de profundidad.
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	_shaderProgram.Activate();
-	_mesh.Draw(GL_TRIANGLES);
-	_shaderProgram.Deactivate();
-	glutSwapBuffers();
-}
-
-int main(int argc, char* argv[])
-{
-	// Inicializamos freeglut. 
-	// Freeglut se encarga de generar y manejar ventanas.
-	// Utilizamos freeglut para crear una ventana donde vamos a dibujar.
-	glutInit(&argc, argv);
-	// Tenemos que informar que queremos trabajar con el pipeline
-	// programable (OpenGL moderno).
-	glutInitContextProfile(GLUT_CORE_PROFILE);
-	// Freeglut nos permite configurar eventos que ocurren en la ventana.
-	// Un evento que nos interesa es cuando alguien cierra la ventana.
-	// En este caso, simplemente dejamos de renderear y terminamos el programa.
-	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
-	// Configuramos el framebuffer. En este caso estamos solicitando un buffer
-	// de color true color RGBA, un buffer de profundidad y un segundo buffer 
-	// para rendererar (evitamos parpadeos al renderear).
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-	// Solicitamos una ventana de 400x400 pixeles.
-	glutInitWindowSize(400, 400);
-	// Creamos y abrimos la ventana con un título personalizado.
-	glutCreateWindow("Hello World OpenGL");
-	// Asociar una función de render. Esta función se mandará a llamar para 
-	// dibujar un frame.
-	glutDisplayFunc(GameLoop);
-
-	// Inicializamos GLEW. Esta librería se encarga de obtener el API
-	// de OpenGL de nuestra tarjeta de video. SHAME ON YOU MICROSOFT.
-	glewInit();
-
-	// COnfigurar OpenGL. Este es el color por default del buffer de color
-	// en el framebuffer.
-	glClearColor(1.0f, 1.0f, 0.5f, 1.0f);
-
-	// Configurar la memoria que la aplicación va a necesitar.
-	Initialize();
-
-	// Iniciamos la aplicacion gráfica. El main se pausará en estea línea
-	// hasta que se cierre la ventana de OpenGL.
-	glutMainLoop();
-
+int main() {
+	
 	return 0;
 }
