b64dfd2e66280c72b24d2e5f2f8a3d97935f4666
pancho1994 <gogeta_64@hotmail.com>
2017-11-28 19:30:02 -0600

    Create Texture2D.h

---
+++ Texture2D.h
@@ -0,0 +1,17 @@
+#include <string>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+class Texture2D
+{
+public:
+	Texture2D();
+	~Texture2D();
+
+	void LoadTexture(std::string path);
+
+	void Bind();
+	void Unbind();
+private:
+	GLuint _textureId;
+};
