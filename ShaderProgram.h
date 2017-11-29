3ef045f1d39ab564c98e9bb6555dd738aed6df96
pancho1994 <gogeta_64@hotmail.com>
2017-11-28 19:28:58 -0600

    Create ShaderProgram.h

---
+++ ShaderProgram.h
@@ -0,0 +1,32 @@
+#include "Shader.h"
+
+class ShaderProgram
+{
+public:
+	ShaderProgram();
+	~ShaderProgram();
+
+	void CreateProgram();
+	void AttachShader(std::string name, GLenum type);
+	void LinkProgram();
+
+	void Activate();
+	void Deactivate();
+
+	void SetAttribute(GLuint locationIndex, std::string name);
+	void SetUniformf(std::string name, float value);
+	void SetUniformf(std::string name, float x, float y);
+	void SetUniformf(std::string name, float x, float y, float z);
+	void SetUniformf(std::string name, glm::vec3 value);
+	void SetUniformf(std::string name, float x, float y, float z, float w);
+	void SetUniformi(std::string name, int value);
+	void SetUniformMatrix(std::string name, glm::mat3 matrix);
+	void SetUniformMatrix(std::string name, glm::mat4 matrix);
+
+private:
+	void DeleteAndDetachShaders();
+	void DeleteProgram();
+
+	GLuint _programHandle;
+	std::vector<std::unique_ptr<Shader>> _attachedShaders;
+};
